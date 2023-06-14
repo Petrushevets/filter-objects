8b56ad46afa6ee140a14698eae0ed04f1b389e72
Volodymyr Petrushevets <vladimir.petr71@gmail.com>
2023-06-14 09:14:49 +0300

    Added the main plugin file.

---
+++ filter-objects.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Filter Objects
+Description: Filter custom post type by taxonomy and custom status field.
+Version: 1.0.0
+Author: Petrushevets Volodymyr
+Text Domain: filter-objects
+*/
+
+defined( 'ABSPATH' ) || exit;
+const FILTER_OBJECTS_FILE = __FILE__;
